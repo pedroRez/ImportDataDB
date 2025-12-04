cfe23ded5d658502cea87c359954bf5a17c778f7
Raphael Rezende <53958298+Raphael-Rezende@users.noreply.github.com>
2025-12-04 20:05:59 -0300

    Handle running app without package context

--- src/app.py
+++ src/app.py
@@ -1,6 +1,12 @@
 from PySide6.QtWidgets import QApplication
 import sys
-from .ui.main_window import MainWindow
+from pathlib import Path
+
+if __package__:
+    from .ui.main_window import MainWindow
+else:
+    sys.path.append(str(Path(__file__).resolve().parent))
+    from ui.main_window import MainWindow
 
 
 def main() -> int:
